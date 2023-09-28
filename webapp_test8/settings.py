a96d83922188a2eb970ce3cfa531fb1b762209cc
Daniele <busond5@gmail.com>
2023-09-28 18:56:09 +0200

    Second commit

--- webapp_test8/settings.py
+++ webapp_test8/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'skaaltec_test8',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
